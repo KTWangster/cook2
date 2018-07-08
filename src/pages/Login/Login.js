647bde12cdc22ec6c9c94a79fbe1b2ed934d97e3
Katie Wang <ktwang16@mac.com>
2018-07-08 19:20:34 -0400

    resolved merge conflicts

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import {Row, Container } from "../../components/Grid";
 import { UserInput } from "../../components/UserLog";
 
+
+
 class Login extends Component {
+    constructor() {
+		super()
+		this.state = {
+			username: "",
+			password: "",
+			redirectTo: null
+        }
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
+	handleSubmit(event) {
+		event.preventDefault()
+		this.props._login(this.state.username, this.state.password)
+		this.setState({
+			redirectTo: '/userprofile'
+		})
+	}
     render() {
         return (
             <Container fluid>
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -20,6 +20,36 @@ class Register extends Component {
         };
     }
 
+//     super(props)
+//     this.state = {
+//         name: "",
+//         username: "",
+//         password: "",
+//         email: "",
+//         address: "",
+//         city: "",
+//         state: "", 
+//         zip: "",
+//         phone: "",
+//     }
+// };
+
+// handleUser = event => {
+//     this.setState({
+//         user: {
+//         name: this.state.user.name,
+//         username: event.target.value,
+//         password: this.state.user.password,
+//         email: this.state.user.email,
+//         address: this.state.user.address,
+//         city: this.state.user.city,
+//         state: this.state.user.state,
+//         zip: this.state.user.password,
+//         phone: this.state.user.phone
+//         }
+//     });
+// }
+
     createUser = user => {
         var request = new XMLHttpRequest();
         request.open('POST', '/register', true);
@@ -41,40 +71,50 @@ class Register extends Component {
                 <Row>
                 <div className="col-sm-6">
                     <form className="login">
-                        <p>Username</p>
+                            <p>Name</p>
+                                <UserInput 
+                                    name="name"
+                                    placeholder="Name"
+                                />
+                            <p>Username</p>
+                                <UserInput 
+                                    name="username"
+                                    placeholder="&#xf007;   Username"
+                                />
+                            <p>Password</p>
+                                <UserInput 
+                                    name="password"
+                                    placeholder="&#xf023;   Password"
+                                />
+                            <p>Email</p>
+                                <UserInput 
+                                    name="email"
+                                    placeholder="Email"
+                                />
+                            <p>Address</p>
                             <UserInput 
-                                name="username"
-                                placeholder="&#xf007;   Username"
+                                name="address"
+                                placeholder="Address"
                             />
-                        <p>Password</p>
+                            <p>City</p>
+                                <UserInput 
+                                    name="city"
+                                    placeholder="Philadelphia"
+                                />
+                            <p>State</p>
+                                <UserInput 
+                                    name="state"
+                                    placeholder="Pennsylvania"
+                                />
+                            <p>Zip Code</p>
+                                <UserInput 
+                                    name="zipCode"
+                                    placeholder="Zip Code"
+                                />
+                            <p>Phone Number</p>
                             <UserInput 
-                                name="password"
-                                placeholder="&#xf023;   Password"
-                            />
-                        <p>First Name</p>
-                            <UserInput 
-                                name="firstName"
-                                placeholder="First Name"
-                            />
-                        <p>Last Name</p>
-                            <UserInput 
-                                name="lastName"
-                                placeholder="Last Name"
-                            />
-                        <p>City</p>
-                            <UserInput 
-                                name="city"
-                                placeholder="Philadelphia"
-                            />
-                        <p>State</p>
-                            <UserInput 
-                                name="state"
-                                placeholder="PA"
-                            />
-                        <p>Zip Code</p>
-                            <UserInput 
-                                name="zipCode"
-                                placeholder="Zip Code"
+                                name="phonne"
+                                placeholder="xxx-xxx-xxxx"
                             />
                         <button className="btn btn-warning registerBtn" onClick={() => this.createUser(this.state.user)}>Register</button>
                     </form>
